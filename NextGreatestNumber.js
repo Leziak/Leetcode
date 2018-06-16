f152b47cca1188cfb6f9e96ff8ac73711acaac96
Leziak <samo.majoros@gmail.com>
2018-06-16 12:56:29 +0200

    NextGreatestNumber

---
+++ NextGreatestNumber.js
@@ -0,0 +1,12 @@
+const nextGreatestLetter = function(letters, target) {
+    let greater = []
+    letters = letters.map(ltr => ltr.charCodeAt());
+    target = target.charCodeAt();
+    for(let ltr of letters){
+        if(ltr > target) greater.push(ltr)
+    }
+    if(!greater.length) return String.fromCharCode(Math.min(...letters))
+    return String.fromCharCode(Math.min(...greater))
+};
+
+console.log(nextGreatestLetter(["c", "f", "j"], "k"));
