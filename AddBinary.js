29b00d7faecec76437893a5f4a4a0d09c2f57dc8
Leziak <samo.majoros@gmail.com>
2018-06-17 10:28:01 +0200

    AddBinary

---
+++ AddBinary.js
@@ -0,0 +1,26 @@
+const addBinary = function(a, b) {
+    let bigger = a.length >= b.length ? a.split('').reverse().join('') : b.split('').reverse().join('');
+    let smaller = a.length < b.length ? a.split('').reverse().join('') : b.split('').reverse().join('');
+    for(let i = 0;i < bigger.length;i++) smaller += '0'
+
+    let result = '';
+    let carry = false;
+    for(let i = 0; i < bigger.length; i++) {
+        if((bigger[i] != smaller[i]) && !carry) result += '1'
+        else if ((bigger[i] != smaller[i]) && carry) result += '0'
+        else if ((bigger[i] == 1 && smaller[i] == 1) && carry) result += '1'
+        else if ((bigger[i] == 0 && smaller[i] == 0) && !carry) result+= '0'
+        else if ((bigger[i] == 0 && smaller[i] == 0) && carry) {
+            result+= '1'
+            carry = !carry
+        }
+        else {
+            result+= '0'
+            carry = !carry
+        }
+    }
+    if(carry) result+='1'
+    return result.split('').reverse('').join('')
+}
+
+console.log(addBinary('10100', '111111111'))
