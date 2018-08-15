034948daa35aaecac7624e802300e3ba5d719cf2
Leziak <samo.majoros@gmail.com>
2018-08-16 01:47:10 +0200

    Change CountPrimes

--- CountPrimes.js
+++ CountPrimes.js
@@ -1,18 +1,20 @@
 // I am going to redo this later since it's actually too slow to pass the final tests.
 
-const countPrimes = function(n) {
-    let primes = 0;
-    for(let i=2;i<n;i++) {
-        let count = 2;
-        while(true){
-            if(i == count) primes++;
-            if(i % count == 0) break;
-            count++;
-        }
-        count = 2;
-    }
-    return primes;
+var countPrimes = function(n) {
+    let count = 0;
 
+    const is_prime = n => {
+        for(let i = 2; i <= Math.sqrt(n); i++) {
+            if(n % i == 0) return false
+        }
+        return true
+    }
+
+    for(let j = 2; j < n; j++) {
+        if(is_prime(j)) count++
+    }
+
+    return count;
 };
 
 console.log(countPrimes(10));
